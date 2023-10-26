89d4b4fe3c63996c879540adf64f5657f4007b67
Trainer PNT <trainer@piit.local>
2023-10-26 21:24:18 +0600

    Post table create with data show into template

--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-blog_post = [
-    {
-        'title': "ভালো শুরুর পরও বাংলাদেশ ২৫৬",
-        'content': "ঊরুর চোটের কারণে ম্যাচটা খেলছেন না সাকিব আল হাসান। তাতে বিশ্বকাপের মঞ্চে বাংলাদেশ দলকে নেতৃত্ব দেওয়ার সুযোগ আসে নাজমুল হোসেনের। পুনের ব্যাটিং স্বর্গে টস ভাগ্য গেছে তাঁর পক্ষেই।",
-        'author': "লিটন",
-        'post_create_date': " ১৯ অক্টোবর ২০২৩"
-    },
-    {
-        'title': "রবি শাস্ত্রীর মন্তব্য কি তাতাবে আফ্রিদিকে",
-        'content': "এবারের বিশ্বকাপটা মোটেও ভালো যাচ্ছে না শাহিন শাহ আফ্রিদির। এমনকি গত মাসে শ্রীলঙ্কা আর পাকিস্তানে অনুষ্ঠিত এশিয়া কাপেও তিনি খুব একটা ভালো করতে পারেননি। চোটের কারণে দীর্ঘ বিরতিটা কি তাঁর বোলিংয়ে মরচে ধরিয়ে দিল! এমন প্রশ্ন উঠছে।",
-        'author': "শাহিন শাহ আফ্রিদি",
-        'post_create_date': " ১৯ অক্টোবর ২০২৩"
-    },
-    {
-        'title': "Sign Language: Unlock the Deaf Culture",
-        'content': "Language is a way of human communication using symbols such as words and gestures. It helps us express our thoughts, ideas, and emotions to others. There are different forms of language including spoken language, written language, and sign language.",
-        'author': "Maruf",
-        'post_create_date': "October 14, 2023"
-    }
-]
+from .models import Post
 
 
 def home(request):
-    return render(request, 'blog/home.html', {'blog_post': blog_post})
+    posts = Post.objects.all()
+    return render(request, 'blog/home.html', {'posts': posts})
 
 
 def about(request):
