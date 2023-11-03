38c2843d3d21024ea1934aa7d7be645a2ae42f0d
mdsawon650 <ahsawon506@gmail.com>
2023-11-04 02:59:23 +0600

    Finall and Profile Create

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=H:\Maruf\django projects\ptFinalProject\venv
+set VIRTUAL_ENV=D:\python vdo\PyPROJECT\pt-django-project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
