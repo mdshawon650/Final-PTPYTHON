a5807054fe38a3b0f8abc835dde4eca59a2651d4
Trainer PNT <trainer@piit.local>
2023-11-03 21:59:40 +0600

    Blog Post add details

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,33 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Post
+# from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView
 
 
 def home(request):
     posts = Post.objects.all()
     return render(request, 'blog/home.html', {'posts': posts})
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailsView(DetailView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
